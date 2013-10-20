09adc726a02bd92966dd45ab871a91410015473c
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-20 12:57:43 -0400

    added delay for pin, allowed zoom

--- public/js/fBomb.js
+++ public/js/fBomb.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
     zoom: 2,
     center: new google.maps.LatLng(5,-30),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDoubleClickZoom: true,
-    scrollwheel: false,
+    zoomControl: true,
+    // disableDoubleClickZoom: true,
+    // scrollwheel: false,
     disableDefaultUI: true
   };
   return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -39,67 +40,49 @@ var retreiveBombs = function() {
 };
 var mapBomb = function(text, lat, lng){
   // var latlng, marker, text, numBombs, rest;
+  var zoom = map.getZoom();
   var random = Math.random() * 100000;
+  var bombGif = '/img/fbomb.gif?';
+  var signPost = '/img/signPost.png?';
   var bomb = new google.maps.Marker({
-    position: new google.maps.LatLng(lat - 10, lng + 1),
-    icon: '/img/fbomb.gif?'+ random,
+    // position: new google.maps.LatLng(lat - 10, lng + 1),
+    position: new google.maps.LatLng(lat, lng),
+    icon: bombGif + random,
     optimized: false,
     draggable:false,
     zIndex:10,
     map: map
   });
-  bombs.push(bomb);
-  var signPost = '/img/signPost.png?';
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(lat, lng),
     title: text,
     icon: signPost + random,
     raiseOnDrag: false,
     draggable:false,
-    zIndex: 9999,
-    map: map
+    animation: google.maps.Animation.DROP
   });
-  //create an options object
-  var tooltipOptions={
-    marker:marker,
-    content:text,
-    cssClass:'tooltip' // name of a css class to apply to tooltip
-  };
-
-// create the tooltip
-createInfoWindow(marker, text);
-createTooltip(marker,text);
-setTimeout(function(){bomb.setMap(null)},4000);
-// console.log($('img[src="'+marker.icon+'"]'));
-// $('img[src="'+marker.icon+'"]').popover({pacement:'top', trigger: 'click', title:text});
-// $('img[src="'+marker.icon+'"]').popover('show');
-
-}
-
-var lastOpenInfoWin = null;
-function createInfoWindow(marker, text) {
-  //create an infowindow for this marker
-  var infowindow = new google.maps.InfoWindow({
-    content: text,
-    maxWidth:150
-  });
-  //open infowindo on click event on marker.
-  google.maps.event.addListener(marker, 'click', function() {
-    if(lastOpenInfoWin) lastOpenInfoWin.close();
-    lastOpenInfoWin = infowindow;
-    infowindow.open(marker.get('map'), marker);
-  });
-}
-function createTooltip(marker, key) {
-  //create a tooltip 
-  var tooltipOptions={
-    marker:marker,
-    map:map,
-    content:key,
-    cssClass:'tooltip' // name of a css class to apply to tooltip
-  };
-  var tooltip = new Tooltip(tooltipOptions);
+  
+    // create the tooltip
+    createInfoWindow(marker, text);
+    setTimeout(function(){
+      bomb.setMap(null);
+      marker.setMap(map);
+    }, 2500);
 }
 
 
 
+  var lastOpenInfoWin = null;
+  function createInfoWindow(marker, text) {
+    //create an infowindow for this marker
+    var infowindow = new google.maps.InfoWindow({
+      content: text,
+      maxWidth:150
+    });
+    //open infowindo on click event on marker.
+    google.maps.event.addListener(marker, 'click', function() {
+      if(lastOpenInfoWin) lastOpenInfoWin.close();
+      lastOpenInfoWin = infowindow;
+      infowindow.open(marker.get('map'), marker);
+    });
+  }
