d3a75bf3170a749fe332be8f8dc91059e954f16a
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-19 20:57:35 -0400

    fixed bomb positioning, removed zoom, pushpin

--- public/js/fBomb.js
+++ public/js/fBomb.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
   var mapOptions;
   mapOptions = void 0;
   mapOptions = {
-    zoom: 3,
-    center: new google.maps.LatLng(30.297018, 0.851440),
+    zoom: 2,
+    center: new google.maps.LatLng(5,-30),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true,
-    disableDoubleClickZoom: true,
-    zoomControl: true
+    disableDefaultUI: true
   };
   return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 });
@@ -24,15 +22,19 @@ var retreiveBombs = function() {
     var latlng, marker, text;
     for (var i = 0; i < data.length; i++) {
       text = data[i].text;
-      latlng = new google.maps.LatLng(data[i].coordinates[0], data[i].coordinates[1]);
       var random = Math.random() * 100000;
-      marker = new google.maps.Marker({
-        position: latlng,
+      bomb = new google.maps.Marker({
+        position: new google.maps.LatLng(data[i].coordinates[0] - 10, data[i].coordinates[1] + 1),
         title: text,
         icon: '/img/fbomb.gif?'+ random,
-        optimized: false
+        optimized: false,
+        map: map
       });
-      marker.setMap(map);
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data[i].coordinates[0], data[i].coordinates[1]),
+        title: text,
+        map: map
+      }); 
     }
     });
 };
