9eb2b8af50af5333108ad81b244c58e6d3485c63
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-28 11:11:52 -0400

    newrelic

--- newrelic.js
+++ newrelic.js
@@ -8,17 +8,17 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['FBomb'],
+  app_name : ['Fbomb.co'],
   /**
    * Your New Relic license key.
    */
-  license_key : 'process.env.LICENSE_KEY',
+  license_key : '51f0000fd5e0f088c088c9867073f0f3c524e5cb',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'trace'
+    level : 'info'
   }
 };
