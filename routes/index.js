ee6ea302a7a985242b7299ee3d0a09e19d420658
Martin Gingras <martin.p.gingras@gmail.com>
2013-11-10 13:18:44 -0500

    gmaps api key in env now

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'FBomb' });
+  res.render('index', { title: 'FBomb', api: process.env.gmaps });
 };
 
 exports.data = function(req, res){
